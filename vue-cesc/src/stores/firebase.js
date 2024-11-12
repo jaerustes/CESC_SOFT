6084ec444b801042ed94d80e4f9151ad18507d9f
Jose Antonio <jaerustes@iesjosepsuredaiblanes.com>
2024-11-12 13:40:39 +0100

    ignores

--- vue-cesc/src/stores/firebase.js
+++ vue-cesc/src/stores/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Configuración de Firebase, usa tus propias credenciales
 const firebaseConfig = {
-    apiKey: "AIzaSyB9Rp-LgGxzkevj6EjDGOvk8SdaFzG0avQ",
-    authDomain: "hist-esc.firebaseapp.com",
+    apiKey: import.meta.env.apiKey,
+    authDomain: import.meta.apiKey.authDomain,
     databaseURL: "https://hist-esc-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "hist-esc",
     storageBucket: "hist-esc.appspot.com",
     messagingSenderId: "530962055028",
-    appId: "1:530962055028:web:62d701b1fdb81cb931f621"
+    appId: import.meta.env.appId
   };
 
 // Inicializa Firebase
